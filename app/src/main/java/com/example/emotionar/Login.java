fa0b103b74b6fc64614f86573cbc318cb5955656
barlakshan <ravindu.ba.lakshan@gmail.com>
2024-06-05 21:13:37 +0530

    Emotioner(register)_1.2

--- app/src/main/java/com/example/emotionar/Login.java
+++ app/src/main/java/com/example/emotionar/Login.java
@@ -1,6 +1,9 @@
 package com.example.emotionar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +13,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
 
+    EditText username,password,repassword;
+    Button signup,signin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        signup=findViewById(R.id.signup);
+        signin=findViewById(R.id.signin);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
