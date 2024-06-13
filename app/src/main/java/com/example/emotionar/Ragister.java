072c678dea18adb619664dbfb255a4bec04eb79a
barlakshan <ravindu.ba.lakshan@gmail.com>
2024-06-13 08:06:18 +0530

    Emotioner(register)2.0

--- app/src/main/java/com/example/emotionar/Ragister.java
+++ app/src/main/java/com/example/emotionar/Ragister.java
@@ -1,24 +1,63 @@
 package com.example.emotionar;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Ragister extends AppCompatActivity {
 
+    EditText username, password, repassword;
+    Button signup;
+    DBHelper DB;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ragister);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        repassword = findViewById(R.id.repassword);
+        signup = findViewById(R.id.signup);
+        DB = new DBHelper(this);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String repass = repassword.getText().toString();
+
+                if (TextUtils.isEmpty(user) || TextUtils.isEmpty(pass) || TextUtils.isEmpty(repass)) {
+                    Toast.makeText(Ragister.this, "All fields Required", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (pass.equals(repass)) {
+                        Boolean checkuser = DB.checkusername(user);
+                        if (!checkuser) {
+                            Boolean insert = DB.insertData(user, pass);
+                            if (insert) {
+                                Toast.makeText(Ragister.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), Login.class);  // Redirect to login activity
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(Ragister.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(Ragister.this, "User already Exists", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(Ragister.this, "Passwords are not matching", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
         });
     }
 }
